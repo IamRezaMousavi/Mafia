badeeff693ec1e49401e1bc833b555eb193b4933
Reza Mousavi <iamrezamousavi@gmail.com>
2024-01-31 00:20:08 +0330

    first commit

---
+++ app/src/main/java/com/github/iamrezamousavi/mafia/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.github.iamrezamousavi.mafia
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.github.iamrezamousavi.mafia.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.textField.setEndIconOnClickListener {
+            val message = binding.textField.editText?.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+
+        binding.topAppBar.setNavigationOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/github/iamrezamousavi/mafia/SettingsActivity.kt
@@ -0,0 +1,22 @@
+package com.github.iamrezamousavi.mafia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.github.iamrezamousavi.mafia.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.settingItem1.settingsItemSwitch.setOnCheckedChangeListener { _, isChecked ->
+            val message = if (isChecked) "Switch1:ON" else "Switch1:OFF"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
