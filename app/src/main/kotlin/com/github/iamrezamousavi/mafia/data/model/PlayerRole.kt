9faf2508d3890d0da31e79d085da23b6f12fc71c
Reza Mousavi <iamrezamousavi@gmail.com>
2025-07-12 17:10:55 +0330

    Minor Changes

--- app/src/main/kotlin/com/github/iamrezamousavi/mafia/data/model/PlayerRole.kt
+++ app/src/main/kotlin/com/github/iamrezamousavi/mafia/data/model/PlayerRole.kt
@@ -1,9 +1,17 @@
 package com.github.iamrezamousavi.mafia.data.model
 
+import com.github.iamrezamousavi.mafia.utils.SIMPLE_CITIZEN
+
 data class PlayerRole(
     val id: String,
     val player: Player,
     val role: Role,
     var isAlive: Boolean = true,
     var showRole: Boolean = false
+)
+
+fun PlayerRole?.orDefault() = this ?: PlayerRole(
+    id = "",
+    player = Player(id = "", name = ""),
+    role = SIMPLE_CITIZEN
 )
